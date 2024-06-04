a37466fc3fe422bdc476922047ae4a84f4466510
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-06-04 15:38:53 +0200

    Random event creation with Faker
    + some test with delete, search and save

--- src/main/java/alessiovulpinari/Application.java
+++ src/main/java/alessiovulpinari/Application.java
@@ -1,14 +1,50 @@
 package alessiovulpinari;
 
+import alessiovulpinari.dao.EventoDao;
+import alessiovulpinari.entities.EventType;
+import alessiovulpinari.entities.Evento;
+import com.github.javafaker.Faker;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("u1-w3-d2-Java");
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        EventoDao eventoDao = new EventoDao(entityManager);
+
+//        for (int i = 0; i < 20; i++) {
+//            Evento evento = CreateEvent();
+//
+//            eventoDao.save(evento);
+//        }
+
+//        eventoDao.deleteEventById(121L);
+
+        Evento searchEvent = eventoDao.getEventById(120L);
+
+        System.out.println(searchEvent);
+    }
+
+    public static Evento CreateEvent() {
+
+        Supplier<Evento> eventSupplier = () -> {
+            Faker faker = new Faker();
+            Random random = new Random();
+            EventType[] eventType = {EventType.PRIVATE, EventType.PUBBLIC};
+            return new Evento(faker.artist().name(), LocalDate.now().minusDays(random.nextInt(0, 40)),
+                    faker.ancient().primordial(), eventType[random.nextInt(2)],
+                    random.nextInt(1000, 5200000));
+        };
+
+        return eventSupplier.get();
     }
 }
